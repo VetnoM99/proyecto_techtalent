8b00afc3a47691e346061966cce1fca6f3a9a5f9
Jordi Exposito <exjoru220806@gmail.com>
2024-07-10 17:18:34 +0200

    Arreglado react

--- frontEnd/src/App.tsx
+++ frontEnd/src/App.tsx
@@ -1,32 +1,28 @@
-
 import React from 'react';
-import './App.css';
+import './styles/App.css';
 import NavBar from './components/NavBar';
 import FooterBar from './components/FooterBar';
-import { BrowserRouter as  Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Login from './Login.tsx';
-
-
-
 
 const App: React.FC = () => {
   return (
     <div className="app-container">
-      <NavBar />
-      <div className="content">
-      <main>
-          <Routes>
-            <Route path="/login" element={<Login />} />
-          </Routes>
-          <Link to="/login">
-            <button>Ir a Login</button>
-          </Link>
-        </main>
-      </div>
-      <FooterBar />
+      <Router>
+        <NavBar />
+        <div className="content">
+          <main>
+            <Routes>
+              <Route path="/login" element={<Login />} />
+            </Routes>
+            <Link to="/login">
+              <button>Ir a Login</button>
+            </Link>
+          </main>
+        </div>
+        <FooterBar />
+      </Router>
     </div>
-    
-    
   );
 };
 
