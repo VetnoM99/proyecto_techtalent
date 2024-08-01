e4ec128bdd326baf9c460874c56257b38534b5d8
Jordi Exposito <exjoru220806@gmail.com>
2024-08-01 12:10:03 +0200

    Footer

--- frontEnd/src/pages/Participa.tsx
+++ frontEnd/src/pages/Participa.tsx
@@ -1,5 +1,5 @@
-// src/components/ParticipateSection.tsx
 import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../styles/ParticipateSection.css'; // Importar el archivo CSS
 
 // Importar las imágenes
@@ -14,6 +14,8 @@ const images = {
 };
 
 const ParticipateSection: React.FC = () => {
+    const navigate = useNavigate();
+
     useEffect(() => {
         const handleScroll = () => {
             const sections = document.querySelectorAll('.section-title, .section-intro, .section-with-background, .section-inner-content, .section-image');
@@ -37,34 +39,41 @@ const ParticipateSection: React.FC = () => {
         };
     }, []);
 
+    const handleButtonClick = () => {
+        navigate('/proyecto');
+    };
+
     return (
         <div className="participate-section">
-            <h2 className="section-title">Participa y Ayuda</h2>
+            <h2 className="section-title">Participa y ayuda</h2>
             <div className="section-content">
                 <p className="section-intro">
                     En PortNet, estamos comprometidos con la protección de nuestros océanos y la vida marina. A través de nuestro programa, puedes convertir tus puntos en donaciones directas a ONG dedicadas a combatir la contaminación marina y la pesca no sostenible. ¡Juntos podemos hacer una gran diferencia!
                 </p>
 
                 <div className="how-to-participate">
-                    <SectionWithBackground title="Gana Puntos" image={images.gainPoints}>
+                    <SectionWithBackground title="Gana puntos" image={images.gainPoints}>
                         <div>
-                            <p>Compra Productos Sostenibles: Cada compra que realizas en nuestra tienda suma puntos a tu cuenta.</p>
-                            <p>Participa en Eventos: Únete a nuestros eventos y campañas, y gana puntos adicionales.</p>
-                            <p>Recicla y Reporta: Envía tus reciclables a nuestros centros de recolección y gana puntos al reportar tus actividades de limpieza.</p>
+                            <p>Compra productos sostenibles: Cada compra que realizas en nuestra tienda suma puntos a tu cuenta.</p>
+                            <p>Participa en eventos: Únete a nuestros eventos y campañas, y gana puntos adicionales.</p>
+                            <p>Recicla y reporta: Envía tus reciclables a nuestros centros de recolección y gana puntos al reportar tus actividades de limpieza.</p>
                         </div>
                     </SectionWithBackground>
 
-                    <SectionWithBackground title="Conviértete en Donador" image={images.becomeDonor}>
+                    <SectionWithBackground title="Conviértete en donador" image={images.becomeDonor}>
                         <div>
-                            <p>Transforma tus Puntos: Una vez que hayas acumulado puntos, puedes convertirlos en donaciones monetarias para las ONG de tu elección.</p>
+                            <p>Transforma tus puntos: Una vez que hayas acumulado puntos, puedes convertirlos en donaciones monetarias para las ONG de tu elección.</p>
                             <p>Elige la ONG: Selecciona entre una lista de organizaciones dedicadas a la conservación marina y la lucha contra la pesca ilegal.</p>
                         </div>
                     </SectionWithBackground>
 
-                    <SectionWithBackground title="Involúcrate Más Allá de los Puntos" image={images.getInvolved}>
+                    <SectionWithBackground title="Involúcrate más allá de los puntos" image={images.getInvolved}>
                         <div>
                             <p>Voluntariado: Únete a nuestras iniciativas de voluntariado para limpiar playas y proteger la vida marina.</p>
-                            <p>Difunde el Mensaje: Comparte nuestra misión y actividades en tus redes sociales para crear conciencia y atraer más colaboradores.</p>
+                            <p>Difunde el mensaje: Comparte nuestra misión y actividades en tus redes sociales para crear conciencia y atraer más colaboradores.</p>
+                            <button className="project-button" onClick={handleButtonClick}>
+                                Ir al Proyecto
+                            </button>
                         </div>
                     </SectionWithBackground>
                 </div>
